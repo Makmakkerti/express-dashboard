d1bce0fab55906d580f9ef346db5a9707815d94b
Makmakkerti <makmakkerti007@gmail.com>
2022-10-15 22:03:09 +0300

    Logger service DI added

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import express, { Express } from 'express';
 import { userRouter } from './users/users';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
 
 export class App {
 	app: Express;
 	port: number;
 	server: Server | undefined;
+	logger: LoggerService;
 
-	constructor(port: number) {
+	constructor(port: number, logger: LoggerService) {
 		this.app = express();
 		this.port = port;
+		this.logger = logger;
 	}
 
 	useRoutes() {
 		this.app.get('/', (req, res) => {
+			this.logger.log('healthcheck');
 			res.send('Welcome to Dashboard');
 		});
 		this.app.use('/user', userRouter);
@@ -22,6 +26,6 @@ export class App {
 	public async init() {
 		this.useRoutes();
 		this.server = this.app.listen(this.port);
-		console.log(`Server running on port ${this.port}`);
+		this.logger.log(`Server running on port ${this.port}`);
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { App } from './app';
+import { LoggerService } from './logger/logger.service';
 
 const bootstrap = async () => {
-	const app = new App(3000);
+	const app = new App(3000, new LoggerService());
 	await app.init();
 };
 
